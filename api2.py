b8e60a7d60ff0507a1cc56bf739c0da60cfbd16c
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2025-03-03 17:53:48 +0300

    create __main__

---
+++ api2.py
@@ -0,0 +1,32 @@
+from io import BytesIO
+
+import requests
+from PIL import Image
+
+
+class YandexAPI:
+    def __init__(self):
+        self.search_api_server = "https://search-maps.yandex.ru/v1/"
+        self.api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
+        self.apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+        self.address_ll = "50.842588,58.846908"
+        self.map_params = {
+                # позиционируем карту центром на наш исходный адрес
+                "ll": self.address_ll,
+                "spn": ",".join(['0.01', '0.01']),
+                "apikey": self.apikey,
+                # добавим точку, чтобы указать найденную аптеку
+            }
+        self.map_api_server = "https://static-maps.yandex.ru/v1"
+        # ... и выполняем запрос
+        response = requests.get(self.map_api_server, params=self.map_params)
+        self.map = Image.open(BytesIO(response.content))
+
+    def get_map(self):
+        return self.map
+
+
+if __name__ == "__main__":
+    map = YandexAPI()
+
+
