99508dd4a6393e91fe7a6d84416000a0341adf27
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2025-03-03 17:24:42 +0300

    create __main__

--- mein.py
+++ mein.py
@@ -1,7 +1,20 @@
 # from PyQt6 import
+import sys
+
+from PyQt6.QtWidgets import QApplication
+from PyQt6.uic.properties import QtCore
+
 from windows import MainWindow
 from api import YandexAPI
 
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == "__main__":
-    pass
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    # win.setWindowFlag(QtCore.Qt.WindowType.WindowStaysOnTopHint)
+    win.show()
+    sys.exit(app.exec())
