90a9ec31d7a557b36168116343c2c92765dc72da
Porog <anatolijmordvin31@gmail.com>
2025-03-03 17:23:49 +0300

    Окно обновление

--- windows.py
+++ windows.py
@@ -1,2 +1,22 @@
-class MainWindow():
-    pass
+import sys
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QLabel, QPushButton
+from window import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.map.setText("map")
+
+    def show_map(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
